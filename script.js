7783407293ce4d6603cde2f4ec24f75d7a13b6c5
FSMDasanlo <jesus.samperruiz@gmail.com>
2025-10-15 21:18:46 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function irAJuegos() {
+  const overlay = document.querySelector('.overlay');
+  overlay.classList.add('fade-out'); // Aplicamos la clase de desvanecimiento
+  setTimeout(() => {
+    window.location.href = "juegos.html"; // Redirige tras 1 segundo
+  }, 1000);
+}
+
+// Redirigir automáticamente tras 10 segundos
+setTimeout(irAJuegos, 10000);
